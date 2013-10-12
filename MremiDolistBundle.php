1efbbeaf63e5f33b0949f08ac3515473d0bc6427
Rémi Marseille <marseille.remi@gmail.com>
2013-10-12 19:59:29 +0200

    Added copyrights

--- MremiDolistBundle.php
+++ MremiDolistBundle.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Mremi\DolistBundle Symfony bundle.
+ *
+ * (c) Rémi Marseille <marseille.remi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Mremi\DolistBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
